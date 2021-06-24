28d26e58eb3695839721ec1e8ffe81d849405a5d
Miheev Vlad <74181151+miheev-vlad@users.noreply.github.com>
2021-06-24 08:57:00 +0500

    update service ts

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -17,16 +17,6 @@ export class HttpService {
     const headers = { 'Authorization': 'Token b556bbb8806ac03e4bec2fc510499aa7f49c67ef', 'content-type': 'application/json', 'accept': 'application/json' };
     const body = JSON.stringify({ query: gstr });
     return this.http.post<ISuggs[]>('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', body, { headers })
-    // return fetch('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', {
-    //   method: "POST",
-    //   mode: "cors",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "Accept": "application/json",
-    //     "Authorization": "Token " + "b556bbb8806ac03e4bec2fc510499aa7f49c67ef"
-    //   },
-    //   body: JSON.stringify({ query: gstr })
-    // })
   }
   getRF(): Observable<IRf[]> {
     return this.http.get<IRf[]>(this._url);
