71db2c1438dd4910c18dbe6c084fc5772386bbcc
Miheev Vlad <74181151+miheev-vlad@users.noreply.github.com>
2021-06-24 08:52:06 +0500

    update home ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-// interface suggsInterface {
-//   value: string;
-//   unrestricted_value: string;
-//   data: Object;
-// }
-
-// const suggsArr: suggsInterface[] = [
-//   {
-//     value: "Павел1", unrestricted_value: "Павел", data: {}
-//   },
-//   {
-//     value: "Павел2", unrestricted_value: "Павел", data: {}
-//   },
-//   {
-//     value: "Павел3", unrestricted_value: "Павел", data: {}
-//   }
-// ]
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -30,67 +12,4 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
-  // public suggs = [] as any;
-  // public topics = [] as any;
-  // get firstName() {
-  //   return this.questionnaireForm.get('firstName');
-  // }
-  // get subscribe() {
-  //   return this.questionnaireForm.get('subscribe');
-  // }
-  // get email() {
-  //   return this.questionnaireForm.get('email');
-  // }
-  // get interested() {
-  //   return this.questionnaireForm.get('interested');
-  // }
-  // constructor(private fb: FormBuilder, private _http: HttpService, private router: Router) { }
-  // ngOnInit() {
-  //   this._http.getRF()
-  //     .subscribe(data => this.topics = data);
-  // }
-  // questionnaireForm = this.fb.group({
-  //   firstName: ['', [Validators.required, Validators.minLength(3), forbiddenFirstNameValidator]],
-  //   email: ['', [Validators.maxLength(20), Validators.pattern(/[а-яА-ЯёЁ]/)]],
-  //   subscribe: [false, Validators.requiredTrue],
-  //   patronymic: [''],
-  //   lastName: [''],
-  //   userName: [''],
-  //   password: [''],
-  //   interested: ['Need', forbiddenSelValidator]
-  // });
-  // onSubmit() {
-    // this._http.getApiData().then((response) => {
-    //   response.json().then((data) => {
-    //     console.log(data);
-    //   });
-    // }).catch();
-  //   console.log(JSON.stringify(this.questionnaireForm.value));
-  //   this.questionnaireForm.setValue({
-  //     firstName: '',
-  //     email: '',
-  //     subscribe: false,
-  //     patronymic: '',
-  //     lastName: '',
-  //     userName: '',
-  //     password: '',
-  //     interested: 'Need'
-  //   })
-  //   this.router.navigateByUrl('/succpage');
-  // }
-  // getSearchName() {
-  //   this._http.getApiData(this.questionnaireForm.get('email')?.value)
-  //     .subscribe(data => {
-  //       console.log(this.suggs);
-  //       this.suggs = data;
-  //       console.log(this.suggs);
-        
-  //     });
-    // this._http.getApiData(this.questionnaireForm.get('email')?.value).then((response) => {
-    //   response.json().then((data) => {
-    //     console.log(data);
-    //   });
-    // }).catch();
-  // }
 }
